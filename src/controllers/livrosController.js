57c25d54cb1b7aaecde2bfb62454120a1298bbdc
leviferre1ra <leviferreira1677@gmail.com>
2026-01-09 09:43:58 -0300

    feat: ordenando os resultados e reutilizando paginação de uma rota

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,29 +1,15 @@
 import NaoEncontrado from "../erros/NaoEncontrado.js";
-import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js"
 import { autores, livros } from "../models/index.js";
 
 class LivroController {
 
   static listarLivros = async (req, res, next) => {
     try {
-      let { limite = 5, pagina = 1 } = req.query;
+      const buscaLivros = livros.find();
 
-      limite = parseInt(limite);
-      pagina = parseInt(pagina);
+      req.resultado = buscaLivros;
 
-      if (limite > 0 && pagina > 0){ 
-      const livrosResultado = await livros.find()
-        .skip((pagina - 1) * limite)
-        .limit(limite)
-        .populate("autor")
-        .exec();
-
-      res.status(200).json(livrosResultado);
-      } else {
-        next(new RequisicaoIncorreta());
-
-      }
-
+      next();   
     } catch (erro) {
       next(erro);
     }
@@ -102,11 +88,13 @@ class LivroController {
 
       if(busca !== null){
 
-      const livrosResultado = await livros
+      const livrosResultado =  livros
       .find(busca)
       .populate("autor");
 
-      res.status(200).send(livrosResultado);
+      req.resultado = livrosResultado
+
+      next();
       } else {
         res.status(200).send([]);
         }
